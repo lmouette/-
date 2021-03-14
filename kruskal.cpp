c46bdf2e34d2eef3ff7850308298314365409857
lmouette <80245953+lmouette@users.noreply.github.com>
2021-03-15 00:09:59 +0800

    Add files via upload

---
+++ kruskal.cpp
@@ -0,0 +1,163 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MaxVetexNum 100
+
+//顶点结构
+typedef char VertexType;
+
+//边结构
+typedef struct{
+    VertexType beginVex;
+	VertexType endVex;
+	int weight;
+}Edge;
+
+//图结构
+typedef struct{
+
+	int vexnum;//顶点数
+	int edgenum;//边数
+	VertexType vertex[MaxVetexNum];//顶点数组
+	Edge edge[MaxVetexNum];//边数组
+
+}Graph;
+void CreatGraph(Graph *g);
+void SortWeight(Graph *g);
+int LocationOfVex(Graph *g,VertexType vex);
+int  Root(int parent[],int currentNode);
+void Kruskal(Graph *g);
+
+
+int main(){
+	Graph g;
+	CreatGraph(&g);
+	
+	Kruskal(&g);
+	return 0;
+}
+
+
+//通过键盘输入初始化无向图
+void CreatGraph(Graph *g)
+{   
+	printf("请依次输入无向图的顶点数和边数：");
+	scanf("%d %d",&g->vexnum,&g->edgenum);
+	getchar();
+	printf("请依次输入每个顶点(回车结束)\n");
+	for(int i= 0;i<g->vexnum;i++){
+		
+		printf("第%d个顶点：",i+1);
+		scanf("%c",&g->vertex[i]);
+		getchar();
+	}
+	printf("请依次输入每条边的两个顶点及边的权重(格式例如：a,c,3)：\n");
+	for (int j=0;j<g->edgenum;j++){
+		
+		printf("第%d条边：",j+1);
+		scanf("%c,%c,%d",&g->edge[j].beginVex,&g->edge[j].endVex,&g->edge[j].weight);	
+		getchar();
+	}
+}
+
+
+
+
+//按边的权值给边排序
+void SortWeight(Graph *g){
+	int i;
+	int j;	
+	int mark;
+	int min;
+	Edge temp;
+	for(i=0;i<g->edgenum-1;i++)
+	{   
+		min = g->edge[i].weight;
+	    mark = i;
+		for(j=i+1;j<g->edgenum;j++){
+		    if(g->edge[j].weight<min){
+			    min = g->edge[j].weight;
+			    mark = j;
+	     	}
+		}
+		temp =  g->edge[i];
+		g->edge[i]=g->edge[mark];
+		g->edge[mark] = temp;
+	}
+	
+}
+
+
+//寻找每个顶点的根节点
+int  Root(int parent[],int r){
+    
+	while(parent[r]>-1){
+		r = parent[r];
+	}
+	return r;
+}
+
+
+
+
+//寻找顶点的下标
+int LocationOfVex(Graph *g,VertexType vex){
+
+	
+	for(int i=0;i<g->vexnum;i++){
+		if(g->vertex[i]==vex){
+			return i;
+		}
+
+	}
+	return -1;
+
+
+}
+
+
+void Kruskal(Graph *g)
+{
+	int i;
+	int count;
+	int parent[MaxVetexNum];
+	int rootOfBenginVex,rootOfEndVex; 
+	int locationOfBeginVex,locationOfEndVex; 
+	
+	SortWeight(g);
+	
+	for(i=0;i<g->vexnum;i++)
+	{
+		parent[i]=-1;
+	}
+	
+	printf("最小生成树如下:\n");
+	//3.
+	for(count=0,i=0;i<g->edgenum;i++)
+	{
+		locationOfBeginVex=LocationOfVex(g,g->edge[i].beginVex);
+		locationOfEndVex=LocationOfVex(g,g->edge[i].endVex);
+
+		rootOfBenginVex=Root(parent,locationOfBeginVex);
+		rootOfEndVex=Root(parent,locationOfEndVex);
+	
+		
+		if(rootOfBenginVex!=rootOfEndVex)
+		{
+			printf("<%c,%c> weight=%d\n",g->edge[i].beginVex,g->edge[i].endVex,g->edge[i].weight);
+			parent[rootOfEndVex]=rootOfBenginVex;
+			count++;
+			
+			if(count==g->vexnum-1)
+			{
+				return;
+			}
+		} 
+	}
+	
+}
+
+
+
+
+
+
---
+++ prim.cpp
@@ -0,0 +1,178 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MaxVertexNum 20 //最大顶点数为100
+#define MaxInt 32767 //表示最大整数，表示 ∞ 
+
+typedef char VertexType; //顶点结构
+
+typedef struct//图结构（邻接矩阵 ）
+{
+	VertexType vertex[MaxVertexNum];
+	int AdjMatrix[MaxVertexNum][MaxVertexNum];
+	int vexnum,arcnum;
+}Graph;
+
+typedef struct
+{
+	VertexType adjvex;
+	int lowcost;
+}ShortEdge;
+
+int LocateVex(Graph *G,VertexType v)
+{
+	int i;
+	
+	for(i=0;i<G->vexnum;i++)
+	{
+		if(v==G->vertex[i])
+		{
+			return i; 
+		} 
+	 } 
+	 
+	 printf("No Such Vertex!\n");
+	 return -1;
+}
+
+void CreateUDN(Graph *G)//构建无向网（Undirected Network）
+{
+	int i,j;
+	//1.输入顶点数和边数
+	printf("输入顶点个数和边数：\n");
+	printf("顶点数 n="); 
+	scanf("%d",&G->vexnum);
+	printf("边  数 e="); 
+	scanf("%d",&G->arcnum);
+	printf("\n"); 
+	
+	printf("\n");
+	
+	//2.输入顶点元素 
+	printf("输入顶点元素(无需空格隔开)：");
+	scanf("%s",G->vertex);
+	printf("\n");
+	//3.矩阵初始化
+	for(i=0;i<G->vexnum;i++) 
+	 for(j=0;j<G->vexnum;j++)
+	    {
+	    	G->AdjMatrix[i][j]=MaxInt;
+		}
+	
+	 //4.构建邻接矩阵
+	 int n,m;
+	 VertexType v1,v2;
+	 int w;//v1->v2的权值 
+	 
+	 printf("请输入边的信息和权值(例：AB,15)：\n");
+	 for(i=0;i<G->arcnum;i++)
+	 {
+	 	printf("输入第%d条边信息及权值：",i+1);
+	 	scanf(" %c%c,%d",&v1,&v2,&w);
+	 	n=LocateVex(G,v1); 
+	 	m=LocateVex(G,v2); 
+	 	
+	 	if(n==-1||m==-1)
+		 {
+		 	printf("NO This Vertex!\n");
+		 	return;
+		  } 
+	
+	   G->AdjMatrix[n][m]=w;
+	   G->AdjMatrix[m][n]=w;//无向网仅此处不同 
+     } 
+}
+
+void print(Graph G)
+{
+	int i,j;
+	printf("\n-------------------------------");
+	printf("\n 邻接矩阵：\n\n"); 	
+	
+		printf("\t ");
+		for(i=0;i<G.vexnum;i++)
+		printf("\t%c",G.vertex[i]);
+		printf("\n");
+		 
+		for(i=0;i<G.vexnum;i++)
+	   {
+	   	  printf("\t%c",G.vertex[i]);
+	   	  
+		  for(j=0;j<G.vexnum;j++)
+	    {
+	    	
+	    	if(G.AdjMatrix[i][j]==MaxInt)
+	 	    printf("\t∞");
+	 	    else printf("\t%d",G.AdjMatrix[i][j]);
+	    }
+	      printf("\n");
+	   }
+
+	 
+}
+
+int minimal(Graph *G,ShortEdge *shortedge)
+{
+	int i,j;
+	int min,loc;
+	
+	min=MaxInt;
+	for(i=1;i<G->vexnum;i++)
+	{
+		if(min>shortedge[i].lowcost&&shortedge[i].lowcost!=0)
+		{
+			min=shortedge[i].lowcost;
+			loc=i;
+		}
+	}
+	return loc;
+}
+ 
+void MiniSpanTree_Prim(Graph *G,VertexType start)
+{ 
+	int i,j,k;
+	ShortEdge shortedge[MaxVertexNum];
+	
+	//1.处理起始点start 
+	k=LocateVex(G,start);
+	for(i=0;i<G->vexnum;i++)
+	{
+		shortedge[i].adjvex=start;
+		shortedge[i].lowcost=G->AdjMatrix[k][i];
+	}
+	shortedge[k].lowcost=0;//lowcost为0表示该顶点属于U集合 
+	
+	//2.处理后续结点 
+	for(i=0;i<G->vexnum-1;i++)
+	{
+		k=minimal(G,shortedge);
+	
+	    printf("%c->%c,%d\n",shortedge[k].adjvex,G->vertex[k],shortedge[k].lowcost);
+	    shortedge[k].lowcost=0;
+	    
+	    for(j=0;j<G->vexnum;j++)
+	    {
+	    	if(G->AdjMatrix[k][j]<shortedge[j].lowcost)
+	    	{
+	    		shortedge[j].lowcost=G->AdjMatrix[k][j];
+	    		shortedge[j].adjvex=G->vertex[k];
+			}
+		}
+	    
+	 } 
+}
+
+
+int main() 
+{
+	VertexType start;
+	 
+	Graph G;
+	CreateUDN(&G);
+	print(G); 
+	
+	printf("请输入起始点：");
+	scanf(" %c",&start);//%c前面有空格 
+	MiniSpanTree_Prim(&G,start);
+	 
+	return 0;
+}
